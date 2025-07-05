0f9d1e252d04c495264c889d9f55127c7526a86a
raldyachmad <achmad.180903@gmail.com>
2025-07-05 12:16:03 +0700

    add app key vercel.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,14 +22,14 @@ class DatabaseSeeder extends Seeder
         // Santri::factory(100)->create();
         // Criteria::factory(5)->create();
 
-        // User::create([
-        //     "name" => "Super Admin",
-        //     "email" => "super@gmail.com",
-        //     "role" => "superadmin",
-        //     "password" => Hash::make('katasandi'),
-        //     'email_verified_at' => now(),
-        //     'remember_token' => Str::random(10),
-        // ]);
+        User::create([
+            "name" => "Super Admin",
+            "email" => "super@gmail.com",
+            "role" => "superadmin",
+            "password" => Hash::make('katasandi'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
         // DB::table('criterias')->insert([
         //     [
         //         'nama' => 'Nilai Raport',
@@ -68,17 +68,17 @@ class DatabaseSeeder extends Seeder
         //     ],
         // ]);
 
-        for ($santriId = 1; $santriId <= 100; $santriId++) {
-            // ID kriteria dari 1 hingga 5
-            for ($kriteriaId = 1; $kriteriaId <= 5; $kriteriaId++) {
-                DB::table('penilaians')->insert([
-                    'santri_id' => $santriId,
-                    'criteria_id' => $kriteriaId,
-                    'nilai' => rand(60, 100), // nilai acak antara 60–100
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-            }
-        }
+        // for ($santriId = 1; $santriId <= 100; $santriId++) {
+        //     // ID kriteria dari 1 hingga 5
+        //     for ($kriteriaId = 1; $kriteriaId <= 5; $kriteriaId++) {
+        //         DB::table('penilaians')->insert([
+        //             'santri_id' => $santriId,
+        //             'criteria_id' => $kriteriaId,
+        //             'nilai' => rand(60, 100), // nilai acak antara 60–100
+        //             'created_at' => now(),
+        //             'updated_at' => now(),
+        //         ]);
+        //     }
+        // }
     }
 }
